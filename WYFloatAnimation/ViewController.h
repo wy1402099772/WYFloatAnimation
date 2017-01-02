d6874e24735e8286dc32f923c9ce31c65bdb5b63
wy1402099772 <1402099772@qq.com>
2017-01-02 22:07:28 +0800

    Initial Commit

---
+++ WYFloatAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYFloatAnimation
+//
+//  Created by wyan assert on 2017/1/2.
+//  Copyright © 2017年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WYFloatAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYFloatAnimation
+//
+//  Created by wyan assert on 2017/1/2.
+//  Copyright © 2017年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
